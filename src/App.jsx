88411f69636d44b64a5e7227a2406d45ba3d45ce
venkatesh <venkatesh.sagard@gmail.com>
2025-11-20 19:48:39 +0530

    changes done to reactjsapp

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,9 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+         <p>
+          welcome to GithubActions kubernetes
+        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
